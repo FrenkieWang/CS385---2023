0ce2454d881779ecd91a1e4d8845ee1d99f935f2
FrenkieWang <FrenkieWang21@gmail.com>
2023-12-10 02:37:01 +0000

    Read Topic 10

--- currentproject/src/routes/PageB.js
+++ currentproject/src/routes/PageB.js
@@ -1,7 +1,10 @@
-function PageB(){
-    return(
-        <h2>This is Page B</h2>
-    );
+function PageB(props) {
+  return (
+    <>
+      <h2>This is Page B</h2>
+      <h3>Variable from home page (parent) has value {props.theVar}</h3>
+    </>
+  );
 }
 
 export default PageB;
--- currentproject/src/routes/PageC.js
+++ currentproject/src/routes/PageC.js
@@ -1,7 +1,10 @@
-function PageC(){
-    return(
-        <h2>This is Page C</h2>
-    );
+function PageC(props) {
+  return (
+    <>
+      <h2>This is Page C</h2>
+      <h3>Variable from home page (parent) has value {props.theVar}</h3>
+    </>
+  );
 }
 
 export default PageC;
